c5a5f761f20ad64d3c281149620add59b837f84c
Steve <Steve@student-net-vpn-3530.intern.ethz.ch>
2023-08-25 23:08:37 +0200

    fix loftr image path

--- localizer.py
+++ localizer.py
@@ -74,11 +74,10 @@ class CameraLocalization:
             np.fill_diagonal(invalid, True)
             pairs = pairs_from_poses.pairs_from_score_matrix(scores, invalid, num_matched)
             pairs = [(images[ids[i]].name, images[ids[j]].name) for i, j in pairs]
-            print(pairs)
-            print(pairs[0])
-            print(pairs[1])
-            # pairs[0] = os.path.join(self.images_ref_relative_path, pairs[0])
-            # pairs[1] = os.path.join(self.images_temp_relative_path, pairs[1])
+            pairs = [(os.path.join(self.images_temp_relative_path, i), 
+                      os.path.join(self.images_ref_relative_path, j)) 
+                      for i, j in pairs]
+            
             for pair in pairs:
                 pairs_total.append(pair)
 
